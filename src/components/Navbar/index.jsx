5d647cafc9f2bad4f7a976c7fb81316e8b7450f0
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-20 18:15:27 +0000

    customised navbar and added project folder and jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,16 +5,19 @@ import Navbar from 'react-bootstrap/Navbar';
 function MyNavbar() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Project Gallery</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">
+            Contact
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
   );
 }
