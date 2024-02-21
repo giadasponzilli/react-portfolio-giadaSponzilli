8ba7828aa9d366bd20d481924d2986176baa7331
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-21 16:42:32 +0000

    added router dependency and router structure to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MyNavbar from './components/Navbar'
+import { Home, Contact, ProjectGallery } from './components/Pages'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <Router basename={'/'}>
       < MyNavbar />
-    </>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='./projectsgallery' element={<ProjectGallery/>}/>
+        <Route path='./contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
   )
 }
 
