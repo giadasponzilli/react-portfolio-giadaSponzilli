018c71eea7a99914518103ba5269b91e4b72fd26
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-22 16:23:31 +0000

    added details in project.json

--- src/components/Pages/ProjectGallery.jsx
+++ src/components/Pages/ProjectGallery.jsx
@@ -1,15 +1,19 @@
 import Project from '../Project';
 import projects from '../../projects.json';
+import './ProjectGallery.css'
 
 const ProjectGallery = () => {
 
   return (
   <>
+  <div className="project-gallery-container">
     <h1>Project Gallery</h1>
-
+    <div className="card-container">
     {projects.map(project => (
       <Project key={project.id} project={project} />
     ))}
+    </div>
+    </div>
   </>
   )
 }
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -3,15 +3,19 @@ import Card from 'react-bootstrap/Card';
 
 function ProjectCard({ project }) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '20rem' }}>
       <Card.Img variant="top" src={project.image} />
       <Card.Body>
         <Card.Title>{project.title}</Card.Title>
         <Card.Text>
           {project.description}
         </Card.Text>
+        <div>
         <a href={project.repository}><Button variant="primary">Repository</Button></a>
+        </div>
+        <div>
         <a href={project.deployedLink}><Button variant="primary">Deployed Site</Button></a>
+        </div>
       </Card.Body>
     </Card>
   );
