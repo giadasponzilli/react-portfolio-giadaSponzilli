e74ccf3ed9410483e51a20a021c08912bb5128f2
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-22 10:57:22 +0000

    changed background image position

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,15 @@
+import './Home.css'; 
+
 const Home = () => {
 
   return (
-  <>
-    <h1>Hi, I am Giada Sponzilli</h1>
-    <h3>I'm a front end webdeveloper based in the United Kingdom</h3>
+  <> 
+    <div className="home-container">
+            <div className="background-image">
+                <h1>Hi, I am Giada Sponzilli</h1>
+                <h3>I'm a front end web developer based in the United Kingdom</h3>
+            </div>
+        </div>
   </>
   )
 }
