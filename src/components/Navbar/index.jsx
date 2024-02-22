16d279143bc3127e85566f8231e6a09a59d54f02
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-22 13:00:57 +0000

    added and changed navbar elements and changed body to block instead of flex

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,23 +1,31 @@
+import { useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './style.css';
+import { NavbarBrand } from 'react-bootstrap';
+import profileImage from '../../assets/images/giadasponzilli-square.jpg';
+
 
 function MyNavbar() {
+
+  // Get current location using useLocation hook to use in the defaultActiveKey for the navbar tabs
+  const location = useLocation();
+  // Extract pathname from location
+  const currentPath = location.pathname;
+
   return (
     <>
-      <Nav variant="tabs" defaultActiveKey="/">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/projectsgallery">Projects Gallery</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/contact">
-            Contact
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navbar fixed="top">
+        <Container fluid>
+          <NavbarBrand className="nameBrand" href="/"><img src={profileImage} alt="Profile" className='profileImage'/>Giada Sponzilli</NavbarBrand>
+          <Nav className="linksWrap" variant="tabs" defaultActiveKey={currentPath}>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/projectsgallery">Projects Gallery</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   );
 }
