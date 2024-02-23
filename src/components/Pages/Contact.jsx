2e224b9db53482f60a4cde7f3fd1d89face4046c
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-23 14:58:39 +0000

    form validation

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -12,11 +12,13 @@ const Contact = () => {
 
   // Setting the component's initial state
   const [formData, setFormData] = useState({
-    name: '',
+    firstName: '',
     lastName: '',
     email: '',
     text: '',
   });
+
+  const [emailSent, setEmailSent] = useState(false);
 
   const handleInputChange = (event) => {
     // Getting the value and name of the input which triggered the change
@@ -34,15 +36,40 @@ const Contact = () => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
 
-    // Alert the user their first and last name, clear `formData.firstName` and `formData.lastName`, clearing the inputs
-    alert(`Hello ${formData.firstName} ${formData.lastName}`);
+    // Validating form fields
+    if (!formData.firstName || !formData.lastName || !formData.email || !formData.text) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    // Validating email format
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(formData.email)) {
+      alert('Please enter a valid email address');
+      return;
+    }
+
+    // Clearing the inputs
     setFormData({
       firstName: '',
       lastName: '',
       email: '',
       text: '',
     });
+
+    setEmailSent(true); //Set emailSent state to true after email is sent
   };
+
+  useEffect(() => {
+    if (emailSent) {
+      const timer = setTimeout(() => {
+        setEmailSent(false);
+      }, 3000); // Set timeout duration (3 seconds)
+
+      // Clear the timer when the component unmounts or when emailSent becomes false again
+      return () => clearTimeout(timer);
+    }
+  }, [emailSent]);
 
   return (
     <>
@@ -55,7 +82,7 @@ const Contact = () => {
           <a className="btn btn-large btn logout customIcon" href="https://uk.linkedin.com/in/giada-sponzilli-b39b9b63" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
 
           <a className="btn btn-large btn logout customIcon" href="mailto:sponzilli.g@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
-          
+
           <h3>Let's connect!</h3>
         </div>
 
@@ -92,6 +119,7 @@ const Contact = () => {
             />
             <button className="form-btn" onClick={handleFormSubmit}>Submit</button>
           </form>
+          {emailSent && <h4>Email has been sent!</h4>}
         </div>
       </div>
     </>
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -5,18 +5,20 @@ function ProjectCard({ project }) {
   return (
     <Card style={{ width: '20rem' }}>
       <div className='image-container'>
-      <Card.Img variant="top" className="cardImg" src={project.image} />
+        <Card.Img variant="top" className="cardImg" src={project.image} />
       </div>
       <Card.Body>
         <Card.Title>{project.title}</Card.Title>
         <Card.Text>
           {project.description}
         </Card.Text>
-        <div>
-        <a href={project.repository}><Button variant="primary">Repository</Button></a>
-        </div>
-        <div>
-        <a href={project.deployedLink}><Button variant="primary">Deployed Site</Button></a>
+        <div className='button-container'>
+          <div>
+            <a href={project.repository}><Button variant="primary">Repository</Button></a>
+          </div>
+          <div>
+            <a href={project.deployedLink}><Button variant="primary">Deployed Site</Button></a>
+          </div>
         </div>
       </Card.Body>
     </Card>
