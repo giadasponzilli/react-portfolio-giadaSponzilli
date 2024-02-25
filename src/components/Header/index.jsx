dcc679b4296f0a6af613bdd69f8d5fae2533e37f
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-25 16:22:39 +0000

    changed home and app window view dimension

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -20,12 +20,14 @@ function MyNavbar() {
       <Navbar fixed="top">
         <Container fluid>
           <NavbarBrand className="nameBrand" href="/"><img src={profileImage} alt="Profile" className='profileImage'/>Giada Sponzilli</NavbarBrand>
+        
           <Nav className="linksWrap" variant="tabs" defaultActiveKey={currentPath}>
             <Nav.Link className="navLink" href="/">Home</Nav.Link>
             <Nav.Link className="navLink" href="/projectsgallery">Projects Gallery</Nav.Link>
             <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
             <Nav.Link className="navLink" href="https://drive.google.com/file/d/1O4xwY41kbWU8C8sS7Dv75uPpkd_pdLE8/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</Nav.Link>
           </Nav>
+
         </Container>
       </Navbar>
     </>
