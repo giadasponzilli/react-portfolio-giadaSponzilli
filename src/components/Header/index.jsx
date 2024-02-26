75f8659ab51689bf5c675d536b5f6a72b7f6c65f
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-26 11:48:30 +0000

    replaced navbar with a collapsible one for mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavbarBrand } from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import profileImage from '../../assets/images/giadasponzilli-square.jpg';
 import './style.css';
@@ -17,7 +18,24 @@ function MyNavbar() {
 
   return (
     <>
-      <Navbar fixed="top">
+      <Navbar fixed="top" expand="lg" className="">
+        <Container fluid>
+          <Navbar.Brand className="nameBrand" href="/"><img src={profileImage} alt="Profile" className='profileImage' />Giada Sponzilli</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="linksWrap mr-auto" variant="tabs" defaultActiveKey={currentPath}>
+              <Nav.Link className="navLink" href="/">Home</Nav.Link>
+              <Nav.Link className="navLink" href="/projectsgallery">Projects Gallery</Nav.Link>
+              <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
+              <Nav.Link className="navLink" href="https://drive.google.com/file/d/1O4xwY41kbWU8C8sS7Dv75uPpkd_pdLE8/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+      {/* <Navbar fixed="top">
         <Container fluid>
           <NavbarBrand className="nameBrand" href="/"><img src={profileImage} alt="Profile" className='profileImage'/>Giada Sponzilli</NavbarBrand>
         
@@ -27,9 +45,10 @@ function MyNavbar() {
             <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
             <Nav.Link className="navLink" href="https://drive.google.com/file/d/1O4xwY41kbWU8C8sS7Dv75uPpkd_pdLE8/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</Nav.Link>
           </Nav>
-
         </Container>
-      </Navbar>
+      </Navbar> */}
+
+
     </>
   );
 }
