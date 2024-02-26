b7a764d28d8a3382a7aa41aa9eb88408c05556c6
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-26 13:10:16 +0000

    added 404 page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MyNavbar from './components/Header'
-import { Home, Contact, ProjectGallery } from './components/Pages'
+import { Home, Contact, ProjectGallery, Custom404 } from './components/Pages'
 import MyFooter from './components/Footer'
 
 import './App.css'
@@ -15,6 +15,7 @@ function App() {
         <Route path='./' element={<Home/>}/>
         <Route path='./projectsgallery' element={<ProjectGallery/>}/>
         <Route path='./contact' element={<Contact/>}/>
+        <Route path='./404' element={<Custom404/>}/>
       </Routes>
       < MyFooter />
     </Router>
---
+++ src/components/Pages/404.jsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+  return (
+    <>
+    <h1>404</h1>
+    </>
+  )
+}
+
+export default Custom404;
