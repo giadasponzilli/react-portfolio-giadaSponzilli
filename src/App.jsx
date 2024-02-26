bf0941f40e126ac8f9e88288cd3bbfe378dbd10e
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-26 19:19:54 +0000

    redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
     >
       < MyNavbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/projectsgallery' element={<ProjectGallery/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/404' element={<Custom404/>}/>
+        <Route path='./' element={<Home/>}/>
+        <Route path='./projectsgallery' element={<ProjectGallery/>}/>
+        <Route path='./contact' element={<Contact/>}/>
+        <Route path='./404' element={<Custom404/>}/>
       </Routes>
       < MyFooter />
     </Router>
