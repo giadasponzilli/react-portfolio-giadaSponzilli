fbd87b109c82c483df972d3cbc726d61e5cf584b
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-26 13:58:17 +0000

    redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MyNavbar from './components/Header'
 import { Home, Contact, ProjectGallery, Custom404 } from './components/Pages'
@@ -15,9 +14,9 @@ function App() {
       < MyNavbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/projectsgallery' element={<ProjectGallery/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/404' element={<Custom404/>}/>
+        <Route path='/react-portfolio-giadaSponzilli/projectsgallery' element={<ProjectGallery/>}/>
+        <Route path='/react-portfolio-giadaSponzilli/contact' element={<Contact/>}/>
+        <Route path='/react-portfolio-giadaSponzilli/404' element={<Custom404/>}/>
       </Routes>
       < MyFooter />
     </BrowserRouter>
