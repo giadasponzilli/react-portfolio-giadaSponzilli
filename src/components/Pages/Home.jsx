c8d8f09bc40c2891ab79fad4b8e404d161ca6d35
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-28 17:48:09 +0000

    projects images in public folder

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import './Home.css';
-import profileImg from '../../assets/images/giadasponzilli-square.jpg'
-
 import Card from 'react-bootstrap/Card';
+
+import profileImg from '../../assets/images/giadasponzilli-square.jpg'
+import './Home.css';
+
 
 const Home = () => {
 
@@ -10,25 +11,20 @@ const Home = () => {
       <div className="home-container">
         <div className="background-image">
 
-          {/* <div className="description-container">
-              <img src="src/assets/images/giadasponzilli-square.jpg" alt="" />
-              <h1>Hi, I am Giada Sponzilli</h1>
-              <h5>As a UK-based Front-end Web Developer with a background in cinematography and photography, I bring a unique blend of creativity and technical expertise to my projects. Currently honing my skills in the EdX Front-end Web Development Bootcamp.</h5>
-          </div> */}
-
-<div className='cardContainerHome'>
-          <Card style={{ width: '22rem' }} className='cardHome'>
-            <div className='image-container'>
-              <Card.Img variant="top" className="cardImg" src={profileImg} alt="" />
-            </div>
-            <Card.Body>
-              <Card.Title className='cardTitle'>Hi, I am Giada Sponzilli</Card.Title>
-              <Card.Text className='cardText'>
-                As a UK-based Front-end Web Developer with a background in cinematography and photography, I bring a unique blend of creativity and technical expertise to my projects. Currently honing my skills in the EdX Front-end Web Development Bootcamp.
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <div className='cardContainerHome'>
+            <Card style={{ width: '22rem' }} className='cardHome'>
+              <div className='image-container'>
+                <Card.Img variant="top" className="cardImg" src={profileImg} alt="" />
+              </div>
+              <Card.Body>
+                <Card.Title className='cardTitle'>Hi, I am Giada Sponzilli</Card.Title>
+                <Card.Text className='cardText'>
+                  As a UK-based Front-end Web Developer with a background in cinematography and photography, I bring a unique blend of creativity and technical expertise to my projects. Currently honing my skills in the EdX Front-end Web Development Bootcamp.
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </div>
+          
         </div>
       </div>
     </>
